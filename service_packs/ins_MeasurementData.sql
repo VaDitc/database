e10b6be8e8d17e872d450e04aba863c5083f8183
Ilchenko Vladyslav <ilchenkovlad@ukr.net>
2017-02-06 18:04:12 +0200

    add script insert data

---
+++ service_packs/ins_MeasurementData.sql
@@ -0,0 +1,21 @@
+USE [scales]
+GO
+/****** Object:  StoredProcedure [dbo].[ins_MeasurementData]    Script Date: 04.02.2017 21:36:29 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[ins_MeasurementData]
+	 @Weight FLOAT,
+	 @ScalesNumber INT,
+	 @Speed FLOAT,
+	 @Productivity FLOAT
+ AS
+ 
+BEGIN
+	SET NOCOUNT ON;
+	insert into MeasurementData (ScalesNumberID, CurrentSpeed, CurrentWeight, CurrentProductivity, TimeOfMeasurement)
+	values (@ScalesNumber, @Speed, @Weight, @Productivity, getdate())
+    SET NOCOUNT OFF;
+END
